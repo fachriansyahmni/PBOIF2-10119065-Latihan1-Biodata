178af296fd0488b17d06b37c5ce3547c43c9f041
fachriansyahmni <fachriansyahmni@gmail.com>
2020-10-17 11:38:13 +0700

    upload latihan1

---
+++ PBOIF2_10119065_Latihan1_Biodata/src/pboif2_10119065_latihan1_biodata/PBOIF2_10119065_Latihan1_Biodata.java
@@ -0,0 +1,23 @@
+package pboif2_10119065_latihan1_biodata;
+/*
+ * @author
+ * NAMA                 : Fachriansyah Muhammad Nur Ihsan
+ * KELAS                : PBOIF2
+ * NIM                  : 10119065
+ * Deskripsi Program    : Program ini menampilkan biodata saya
+ *
+ */
+public class PBOIF2_10119065_Latihan1_Biodata {
+    public static void main(String[] args) {
+        System.out.println("BIODATA ROCKSTAR" + "\n"+"==================");
+        System.out.println("NAMA\t\t: Fachriansyah Muhammad Nur Ihsan");
+        System.out.println("ALAMAT\t\t: Jalan Kamboja 1 no 21");
+        System.out.println("TEMPAT LAHIR\t: Tasikmalaya");
+        System.out.println("TANGGAL LAHIR\t: 24 September 2001");
+        System.out.println("------------------------");
+        System.out.println("HOBI\t\t: ");
+        System.out.println("MAKES\t\t: ");
+        System.out.println("MIKES\t\t: Air Putih");
+    }
+    
+}
